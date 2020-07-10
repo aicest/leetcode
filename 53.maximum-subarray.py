a37c9cbd3ec11aa75085e0fb8354d44fb2ee6014
Fai <aicest@users.noreply.github.com>
2020-07-11 03:01:23 +0800

    refactor(53): 分治法

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -5,11 +5,37 @@
 #
 
 # @lc code=start
+import math
+
+
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        best = curr = nums[0]
-        for i in range(1, len(nums)):
-            curr = max(curr, 0) + nums[i]
-            best = max(best, curr)
-        return best
+    def maxSubArray(self, nums):
+        return self.dac(nums, 0, len(nums) - 1)
+
+    def dac(self, nums, start, end):
+        if start == end:
+            return nums[start]
+
+        if start + 1 == end:
+            return max(nums[start], nums[start] + nums[end], nums[end])
+
+        pivot = math.floor((start + end) / 2)
+        leftSum = rightSum = 0
+        maxLeftSum = maxRightSum = float("-inf")
+
+        for i in range(pivot, start - 1, -1):
+            leftSum += nums[i]
+            maxLeftSum = max(maxLeftSum, leftSum)
+
+        for i in range(pivot + 1, end + 1):
+            rightSum += nums[i]
+            maxRightSum = max(maxRightSum, rightSum)
+
+        return max(
+            self.dac(nums, start, pivot),
+            maxLeftSum + maxRightSum,
+            self.dac(nums, pivot, end),
+        )
+
+
 # @lc code=end
