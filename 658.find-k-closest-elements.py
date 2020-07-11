93483cccb77f56affcb4b5314cae9a66a9e08545
Fai <aicest@users.noreply.github.com>
2020-07-11 22:24:32 +0800

    refactor(658): 二分查找

--- 658.find-k-closest-elements.py
+++ 658.find-k-closest-elements.py
@@ -7,50 +7,28 @@
 # @lc code=start
 class Solution:
     def findClosestElements(self, arr, k, x):
-        pos = self.bs(arr, x, 0, len(arr) - 1)
-        return self.tp(arr, x, pos, k)
+        pos = self.bs(arr, x, k, 0, len(arr) - k)
+        return arr[pos : pos + k]
 
-    def bs(self, arr, x, start, end):
+    def bs(self, arr, x, k, start, end):
         if start == end:
             return start
-        if start + 1 == end:
-            if x - arr[start] <= arr[end] - x:
-                return start
-            else:
-                return end
         pivot = (start + end) // 2
-        if arr[pivot] == x:
+        a = arr[pivot]
+        b = arr[pivot + k]
+        if a == x and x == b:
             return pivot
-        if x < arr[pivot]:
-            if arr[pivot] - x < x - arr[pivot - 1]:
-                return pivot
-            return self.bs(arr, x, start, pivot - 1)
+        if x - a <= b - x:
+            return self.bs(arr, x, k, start, pivot)
         else:
-            if x - arr[pivot] <= arr[pivot + 1] - x:
-                return pivot
-            return self.bs(arr, x, pivot + 1, end)
-
-    def tp(self, arr, x, pos, k):
-        result = [arr[pos]]
-        k -= 1
-        i = pos - 1
-        j = pos + 1
-        n = len(arr)
-        while 0 <= i and j < n and k >= 1:
-            if x - arr[i] <= arr[j] - x:
-                result.insert(0, arr[i])
-                k -= 1
-                i -= 1
-            else:
-                result.append(arr[j])
-                k -= 1
-                j += 1
-        if k >= 1:
-            if 0 <= i:
-                result = arr[i - k + 1 : i + 1] + result
-            else:
-                result = result + arr[j : j + k]
-        return result
+            return self.bs(arr, x, k, pivot + 1, end)
 
 
 # @lc code=end
+
+
+if __name__ == "__main__":
+    print(Solution().findClosestElements([2, 2, 6, 6], 2, 4))
+    print(Solution().findClosestElements([1, 2, 5], 1, 3))
+    print(Solution().findClosestElements([1, 2, 5], 1, 4))
+    print(Solution().findClosestElements([1, 2, 5, 7, 7, 7, 7], 1, 4))
