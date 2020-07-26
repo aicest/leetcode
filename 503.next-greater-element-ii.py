010ed7d3d8caed9c69fdab4f88327e46808a8031
Fai <aicest@users.noreply.github.com>
2020-07-26 17:57:45 +0800

    refactor(503): 栈

--- 503.next-greater-element-ii.py
+++ 503.next-greater-element-ii.py
@@ -7,21 +7,15 @@
 # @lc code=start
 class Solution:
     def nextGreaterElements(self, nums):
-        if not nums:
-            return []
+        # https://www.cnblogs.com/ilovezyg/p/6477503.html
         n = len(nums)
-        result = [None] * n
-        for i in range(n - 1, -1, -1):
-            if nums[i] < nums[i + 1 - n]:
-                result[i] = nums[i + 1 - n]
-                continue
-            for j in range(i + 1 - n, (i + 1 - n) + n - 1):
-                best = nums[j] if result[j] == None else max(nums[j], result[j])
-                if nums[i] < best:
-                    result[i] = best
-                    break
-            else:
-                result[i] = -1
+        result = [-1] * n
+        stack = []
+        for i in range(n * 2):
+            i = i % n
+            while stack and nums[stack[-1]] < nums[i]:
+                result[stack.pop()] = nums[i]
+            stack.append(i)
         return result
 
 
@@ -31,8 +25,13 @@ if __name__ == "__main__":
     import time
 
     start = time.perf_counter()
-    assert [2, -1, 2] == Solution().nextGreaterElements([1, 2, 1])
-    assert [2, -1, -1, 2, 1, 1] == Solution().nextGreaterElements([1, 2, 2, 1, 0, -1])
-    assert [3, 2, 3, 3, -1] == Solution().nextGreaterElements([2, 1, 2, 1, 3])
+    print([-1, 4, 4, 4] == Solution().nextGreaterElements([4, 3, 2, 1]))
+    print([2, 3, 4, -1] == Solution().nextGreaterElements([1, 2, 3, 4]))
+    print([2, -1, 2, -1] == Solution().nextGreaterElements([1, 2, 1, 2]))
+    print([-1, 2, -1, 2] == Solution().nextGreaterElements([2, 1, 2, 1]))
+    print([2, 3, 3, -1] == Solution().nextGreaterElements([1, 2, 1, 3]))
+    print([3, -1, 2, 3] == Solution().nextGreaterElements([1, 3, 1, 2]))
+    print([3, 3, -1, 2] == Solution().nextGreaterElements([2, 1, 3, 1]))
+    print([-1, 2, 3, 3] == Solution().nextGreaterElements([3, 1, 2, 1]))
     end = time.perf_counter()
     print(end - start)
