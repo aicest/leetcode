28425aae87644567a6c79c3263b3465007543da6
Fai <aicest@users.noreply.github.com>
2020-08-02 21:37:23 +0800

    feat(206): 递归

---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+class Solution:
+    def reverseList(self, head):
+        if not head:
+            return None
+        return self.traverse(head)[0]
+
+    def traverse(self, node):
+        if not node.next:
+            return node, node
+        head, tail = self.traverse(node.next)
+        tail.next = node
+        node.next = None
+        return head, node
+
+
+# @lc code=end
+
+from __ListNode import *
+
+if __name__ == "__main__":
+    create = ListNode.create
+    format = ListNode.format
+    print("3->2->1->NULL" == format(Solution().reverseList(create([1, 2, 3]))))
+    print("2->1->NULL" == format(Solution().reverseList(create([1, 2]))))
+    print("1->NULL" == format(Solution().reverseList(create([1]))))
+    print("NULL" == format(Solution().reverseList(create([]))))
