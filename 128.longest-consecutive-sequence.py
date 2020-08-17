c3319a3d03adf2883852a5799e2c319f2a0a4020
Fai <aicest@users.noreply.github.com>
2020-08-17 23:45:49 +0800

    feat(128): 并查集

---
+++ 128.longest-consecutive-sequence.py
@@ -0,0 +1,53 @@
+from typing import *
+
+#
+# @lc app=leetcode id=128 lang=python3
+#
+# [128] Longest Consecutive Sequence
+#
+
+# @lc code=start
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        best = 0
+        tree = {}
+        for x in nums:
+            tree[x] = {"parent": x, "size": 1}
+        for x in nums:
+            best = max(best, tree[x]["size"])
+            if x - 1 in tree:
+                best = max(best, self.union(tree, x, x - 1)["size"])
+            if x + 1 in tree:
+                best = max(best, self.union(tree, x, x + 1)["size"])
+        return best
+
+    def union(self, tree, src, dest):
+        srcRoot, destRoot = self.find(tree, src), self.find(tree, dest)
+        if srcRoot == destRoot:
+            return srcRoot
+        if srcRoot["size"] <= destRoot["size"]:
+            srcRoot["parent"] = destRoot["parent"]
+            destRoot["size"] += srcRoot["size"]
+            return destRoot
+        else:
+            destRoot["parent"] = srcRoot["parent"]
+            srcRoot["size"] += destRoot["size"]
+            return srcRoot
+
+    def find(self, tree, target):
+        while tree[target]["parent"] != target:
+            target, tree[target]["parent"] = (
+                tree[target]["parent"],
+                tree[tree[target]["parent"]]["parent"],
+            )
+        return tree[target]
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    print(0 == Solution().longestConsecutive([]))
+    print(1 == Solution().longestConsecutive([1]))
+    print(1 == Solution().longestConsecutive([1, 3]))
+    print(4 == Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
+    print(4 == Solution().longestConsecutive([100, 4, 200, 2, 1, 3]))
