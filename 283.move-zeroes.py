d5ba2b381535747fea5b2d92d813a50e2eabe247
Fai <aicest@users.noreply.github.com>
2020-08-23 10:30:47 +0800

    refactor(283): 双指针

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -13,6 +13,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i, j, n = 0, 0, len(nums)
+        while j < n:
+            if nums[j] == 0:
+                j += 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i, j = i + 1, j + 1
+
+    def v1(self, nums):
+        i, j, n = 0, 0, len(nums)
         while i < n and j < n:
             while i < n and nums[i] != 0:
                 i += 1
